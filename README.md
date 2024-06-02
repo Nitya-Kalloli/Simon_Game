e796c0bfd81b16242074ad0c0ad214a6c72787a5
Nitya-Kalloli <nityakalloli.dev@gmail.com>
2024-06-02 20:59:23 +0530

    Added Files

---
+++ README.md
@@ -0,0 +1,53 @@
+# Simon Says Game
+
+## Overview
+
+The Simon Says Game is a timeless test of memory and pattern recognition. Players must mimic a sequence of colors presented by the game. As players accurately reproduce each sequence, the game becomes increasingly difficult with each subsequent round.
+
+## Features
+
+- **Responsive Design**: The game adapts to different devices, providing a seamless user experience across all platforms.
+- **Vibrant Interface**: Attractive and eye-catching colors are used to enrich the gaming experience.
+- **Audio Feedback**: Pleasant sound effects are included to give feedback on correct and incorrect actions.
+- **Increasing Difficulty**: The game gets increasingly challenging as the player successfully advances through each level.
+
+## Technologies Used
+
+- **HTML**: Structuring the web page.
+- **CSS**: Styling the user interface.
+- **JavaScript**: Implementing game logic and interactivity.
+
+## How to Play
+
+1. Open the `index.html` file in your web browser.
+2. Press any key to begin the game.
+3. Watch the sequence of colors displayed by Simon.
+4. Click on the colors in the same order to replicate the sequence.
+5. If you make a mistake, the game will notify you, and you can try again.
+6. Successfully completing a round increases the difficulty for the next round.
+7. The game continues until you make an incorrect move.
+
+## File Structure
+
+- `index.html`: The main HTML file that structures the game interface.
+- `style.css`: The CSS file for styling the game.
+- `app.js`: The JavaScript file containing the game logic.
+
+
+## Contributing
+
+Contributions are welcome! If you have any improvements or new features to suggest, please create a pull request.
+
+
+
+Enjoy playing Simon Says! If you have any questions or issues, feel free to [open an issue](https://https://github.com/Nitya-Kalloli).
+
+
+
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at nityakalloli.dev@gmail.com
+
+
---
+++ app.js
@@ -0,0 +1,111 @@
+let gameSeq=[];
+let userSeq=[];
+
+let btns = ["yellow", "red","purple","green"];
+
+let start = false;
+let level =0;
+
+let h2 = document.querySelector("h2");
+ 
+document.addEventListener("keypress", function(){
+    if(start==false)
+    {
+        console.log("Game is started");
+        start=true;
+        levelUp(); 
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash")
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250)
+}
+
+function playSound(){
+    let audio = new Audio('./sounds/green.mp3');
+    audio.play();
+}
+
+
+function userFlash(btn){
+    btn.classList.add("userflash")
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250)
+}
+
+let highScore = 0;
+function levelUp(){
+    userSeq=[];
+    level++;
+    h2.innerText=`Level ${level}. Your High Score is ${highScore}`;
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+    playSound();
+ 
+}
+
+function wrongAudio(){
+    let audio = new Audio('./sounds/wrong.mp3');
+    audio.play();
+}
+
+function checkAns(idx){
+    if(userSeq[idx]==gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+        highScore = level;
+
+    }
+    else
+    { 
+        if(level-1<0){
+        h2.innerHTML=`Game Over! Your Score was <b>${level}</b> <br> Press any key to start. `;
+        wrongAudio();
+        }
+        else{
+        h2.innerHTML=`Game Over! Your Score was <b>${level-1}</b> <br> Press any key to start. `;
+        wrongAudio();
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+        document.querySelector("body").style.backgroundColor="white";
+        document.querySelector("body").style.backgroundColor="#011F3F";
+        },150)
+    }
+        reset();
+
+    }
+}
+
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1)
+     
+}
+
+let allBtns = document.querySelectorAll(".btn"); 
+
+for(btn of allBtns)
+{
+    btn.addEventListener("click",btnPress);
+}
+
+function reset(){
+    start = false;
+    gameSeq=[];
+    userSeq=[];
+    level=0;
+}
